d172ede4ceeb9bb3a4f668ccba57954e34d471e9
33Frok1e <mratikrishna@gmail.com>
2024-10-20 16:52:37 +0530

    Fix Bugs

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Logo from './Logo'
+import Container from './container/Container'
+import LogoutBtn from './Header/LogoutBtn'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+import Login from './Login'
+import Signup from './Signup'
+import PostCard from './PostCard'
+import RTE from './RTE'
+import PostForm from './post-form/PostForm'
+import AuthLayout from '../components/AuthLayout'
+
+export {
+    Logo,
+    Container,
+    LogoutBtn,
+    Header,
+    Footer,
+    Button,
+    Input,
+    Select,
+    Login,
+    Signup,
+    PostCard,
+    RTE,
+    PostForm,
+    AuthLayout
+}
